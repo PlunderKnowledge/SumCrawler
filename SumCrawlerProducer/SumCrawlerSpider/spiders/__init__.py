5a2581b6525f2bab265fe2f277ceca87a0c11905
Greg Rubino <greg.rubino@medialets.com>
2016-05-06 16:03:12 -0400

    make sum crawler recursive

--- SumCrawlerProducer/SumCrawlerSpider/spiders/__init__.py
+++ SumCrawlerProducer/SumCrawlerSpider/spiders/__init__.py
@@ -6,14 +6,24 @@ import scrapy
 import functools
 import requests
 
+def _link_is_http(l):
+    return l.startswith('https://') or l.startswith('http://')
+
+def _link_is_archive(l):
+    return l.endswith('.tar.gz')
+
 class GnuSpider(scrapy.Spider):
     name = "gnu-barrymore"
     allowed_domains = ["gnu.org"]
     start_urls = ["http://www.gnu.org"]
     keyring_url = "http://ftp.gnu.org/gnu/gnu-keyring.gpg"
+    visited = set()
 
     def parse(self, response):
-        archive_urls = [sel.extract() for sel in response.xpath('//a/@href') if sel.extract().endswith('.tar.gz')]
+
+        all_urls = [sel.extract() for sel in response.xpath('//a/@href') if _link_is_http(sel.extract())]
+        
+        archive_urls = [url for url in all_urls if _link_is_archive(sel.extract())]
         sig_urls = [url+".sig" for url in archive_urls]
         for (archive_url, sig_url) in zip(archive_urls, sig_urls):
             item = SumcrawlerspiderItem()
@@ -25,3 +35,10 @@ class GnuSpider(scrapy.Spider):
                 item['signature'] = ''
             item['signatureType'] = 'gpg'
             yield item
+
+        follow_links = [url for url in all_urls if not _link_is_archive(url)]
+        for link in follow_links:
+            if(not link in self.visited):
+                self.visited.add(link)
+                yield scrapy.Request(link, callback=self.parse)
+
